7c47da57388bb93069f9d856429d2e3f9a5ec436
Michael Kleyman <michaelkleyman99@gmail.com>
2023-03-09 08:52:31 -0500

    tweaking calendar styles

--- daygrade/src/components/TempCalendar.js
+++ daygrade/src/components/TempCalendar.js
@@ -56,22 +56,22 @@ export default function Temp() {
 
   const CustomPicker = styled(CalendarPicker)(({ theme }) => ({
     '&.MuiCalendarPicker-root': {
-      width: '70% !important',
-      maxHeight: '600px !important',
-      height: '470px !important',
+      width: '70%',
+      maxHeight: '600px',
+      height: '470px',
     },
     '& .MuiCalendarPicker-viewTransitionContainer': {
       '& .css-sf5t6v-PrivatePickersSlideTransition-root-MuiDayPicker-slideTransition':
         {
-          minHeight: '350px !important',
-          height: 'auto !important',
+          minHeight: '350px',
+          height: 'auto',
         },
       '& .css-qklzlb-MuiDayPicker-header': {
         '& .css-raiqh1-MuiTypography-root-MuiDayPicker-weekDayLabel': {
-          height: '70px !important',
-          width:`${tileSize} !important`,
-          color: 'blue !important',
-          fontWeight: 'bolder !important',
+          height: '70px',
+          width: tileSize,
+          color: 'blue',
+          fontWeight: 'bolder',
         },
       },
     },
